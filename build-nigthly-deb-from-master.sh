72441ebd72e78c37b9ed18b56bbf247f3036551a
Damien Ciabrini <damien.ciabrini@gmail.com>
2019-08-12 15:57:08 -0400

    Branch for building deb packages

---
+++ build-nigthly-deb-from-master.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+export DEBFULLNAME="bot"
+export DEBEMAIL="bot@address.local"
+
+PROJECT=$(basename $(pwd))
+UPSTREAM_VERSION=$(git grep AC_INIT master:configure.ac | sed -ne 's/.*\[\(.*\)\].*/\1/p')
+read DATE SHORTHASH LONGHASH <<<$(git log -1 --date=format:"%Y%m%d%H%M" --pretty=format:"%cd %h %H" master)
+DEB_VERSION=${UPSTREAM_VERSION}~${DATE}.${SHORTHASH}
+
+dch -v ${DEB_VERSION}-1 -U "Nightly build from tag ${LONGHASH}"
+git archive --format=tar --prefix=${PROJECT}-${DEB_VERSION}/ master | gzip -c > ${PROJECT}_${DEB_VERSION}.orig.tar.gz
+tar xf ${PROJECT}_${DEB_VERSION}.orig.tar.gz
+cd ${PROJECT}-${DEB_VERSION}
+cp -a ../debian .
+dpkg-buildpackage -rfakeroot -us -uc
