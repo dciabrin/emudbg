ba949df8b4b7812f76fab42be1732dcab1017e36
Damien Ciabrini <damien.ciabrini@gmail.com>
2023-05-15 12:16:17 +0200

    msys2: move CI build to ucrt64 subsystem
    Ref dciabrin/ngdevkit#92

--- .github/scripts/build-msys2.sh
+++ .github/scripts/build-msys2.sh
@@ -2,7 +2,7 @@
 set -eu
 autoreconf -iv
 ./configure \
-    --prefix=/mingw64 \
+    --prefix=/ucrt64 \
     --build=x86_64-w64-mingw32 \
     --host=x86_64-w64-mingw32 \
     --target=x86_64-w64-mingw32
